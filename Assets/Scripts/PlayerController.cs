b849742954c1bb0462baf637814c11954e2ad70a
Conf327 <conf327@soton.ac.uk>
2020-01-31 23:46:14 +0000

    added player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 4f;
+    public float hspd, vspd = 0;
+    public float horrizontalVel = 0;
+    public float verticalVel = 0;
+
+    private float halfSpeed;
+    private float fullSpeed;
+
+    private LineRenderer lr;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        halfSpeed = speed / 2;
+        fullSpeed = speed;
+        lr = GetComponent<LineRenderer>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horrizontalVel = Input.GetAxisRaw("Horizontal");
+        verticalVel = Input.GetAxisRaw("Vertical");
+
+        if(Mathf.Abs(horrizontalVel) > 0 && Mathf.Abs(verticalVel) > 0)
+        {
+            speed = halfSpeed;
+        }
+        else
+        {
+            speed = fullSpeed;
+        }
+
+        horrizontalVel *= speed;
+        verticalVel *= speed;
+
+        Vector3 pos = gameObject.transform.position;
+        gameObject.transform.position = new Vector3(pos.x + horrizontalVel * Time.deltaTime, pos.y + verticalVel * Time.deltaTime, pos.z);
+
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 realMousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        float AngleRad = Mathf.Atan2(realMousePos.y - gameObject.transform.position.y, realMousePos.x - gameObject.transform.position.x);
+        float angle = (180 / Mathf.PI) * AngleRad;
+
+        float dir = Vector3.Angle(gameObject.transform.position, realMousePos);
+        gameObject.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x, gameObject.transform.rotation.y, angle);
+
+
+
+        RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position,gameObject.transform.right);
+        Vector3 hitPos = Vector3.zero;
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.layer == 8)
+            {
+                hitPos = hit.point;
+            }
+            else
+            {
+                hitPos = gameObject.transform.right * 99999;
+            }
+        }
+        else
+        {
+            hitPos = gameObject.transform.right * 99999;
+        }
+
+        
+
+
+
+
+
+
+        Vector3[] Positions = new Vector3[] { gameObject.transform.position, hitPos};
+        Vector3[] LinePositions = new Vector3[Positions.Length];
+
+
+        for (int i = 0; i < Positions.Length; i++)
+        {
+            Positions[i].z = 2;
+
+        }
+        Color lineColor = new Color(255, 0, 0, 0.5f);
+        lr.alignment = LineAlignment.TransformZ;
+        lr.startColor = lineColor;
+        lr.endColor = lineColor;
+        lr.sortingOrder = 0;
+
+
+        
+
+        
+
+        lr.SetPositions(Positions);
+
+
+    }
+}
