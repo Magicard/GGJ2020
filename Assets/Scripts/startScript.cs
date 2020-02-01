8411a3a3f244d73cafb55c4b1fb485bbb5600ccc
Conf326 <conf326@soton.ac.uk>
2020-02-01 06:01:31 +0000

    fixed camera that kept moving infinitely

--- Assets/Scripts/startScript.cs
+++ Assets/Scripts/startScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class startScript : MonoBehaviour
 {
+    public bool stopMoving = false;
     public float tranSpeed = 1f;
     // Start is called before the first frame update
     void Start()
@@ -14,9 +15,18 @@ public class startScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(transform.position.x, 0f, transform.position.z);
-        Quaternion newRot = Quaternion.Euler(0f, transform.rotation.y, transform.rotation.z);
-        transform.position = Vector3.Slerp(transform.position, newPos, Time.deltaTime * tranSpeed);
-        transform.rotation = Quaternion.Slerp(transform.rotation, newRot, Time.deltaTime * tranSpeed);
+        Invoke("stop", 7);
+        if (stopMoving == false)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, 0f, transform.position.z);
+            Quaternion newRot = Quaternion.Euler(0f, transform.rotation.y, transform.rotation.z);
+            transform.position = Vector3.Slerp(transform.position, newPos, Time.deltaTime * tranSpeed);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRot, Time.deltaTime * tranSpeed);
+        }
+    }
+
+    void stop()
+    {
+        stopMoving = true;
     }
 }
